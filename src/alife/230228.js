a5768537018e4093f95743a9889127ce8e6212d9
negiandleek <17189098+negiandleek@users.noreply.github.com>
2023-03-02 12:31:57 +0000

    2023/03/02

--- src/alife/230228.js
+++ src/alife/230228.js
@@ -1,18 +1,25 @@
 const canvasWidth = 480;
 const canvasHeight = 480;
 
-const initialPlantNum = 1;
+const initialPlantNum = 5;
 const plants = []
 
 class Plant {
 	static maxEnergy = 100;
   static minEnergy = 6;
-  static maxLifeSpan = 1000;
+  static maxLifeSpan = 750;
   static sickProb = 0.01;
   static reproduceBaseCycle = 200
-  constructor(isGermination=false) {
-    this.x = random(width);
-    this.y = random(height);
+  static dispersalDistance = 40
+  static randomPointAround(vector) {
+    let angle = random(TWO_PI);
+    let r = random(Plant.dispersalDistance);
+    let dx = cos(angle) * r;
+    let dy = sin(angle) * r;
+    return createVector(vector.x + dx, vector.y + dy);
+  }
+  constructor(vector, isGermination=false) {
+    this.position = isGermination ? Plant.randomPointAround(vector): vector;
 		this.primeEnergy = randomGaussian(Plant.maxEnergy, 10);
     this.energy = isGermination ? 0 : this.primeEnergy
     this.lifespan = randomGaussian(Plant.maxLifeSpan, 50)
@@ -33,7 +40,7 @@ class Plant {
     if(this.status == 'MATURE'){
       const maturityPeriod = round(this.time - this.lifespan * 0.2)
       if(maturityPeriod % this.reproduceCycle == 0){
-        plants.push(new Plant(true))
+        plants.push(new Plant(this.position, true))
       }
     }
   }
@@ -58,16 +65,17 @@ class Plant {
     const alpha = map(this.energy, 0, this.primeEnergy, 0, 255)
     fill(0,0,0, alpha);
     noStroke();
-    ellipse(this.x, this.y, 10, 10);
+    ellipse(this.position.x, this.position.y, 10, 10);
   }
 }
 
 function setupPlant(){
   for(let i = 0; i < initialPlantNum; i+=1){
-    plants.push(new Plant())
+    plants.push(new Plant(createVector(random(width), random(height))))
   }
 }
 
+let time = 0
 
 function setup() {
 	createCanvas(canvasWidth, canvasHeight);
@@ -75,6 +83,13 @@ function setup() {
 }
 
 function draw(){
+  time++
+  if(time % 100 == 0){
+    console.log(time)
+  }
+  if(time >= 2800){
+    return
+  }
 	background(240);
   for (let plant of plants) {
     plant.draw();

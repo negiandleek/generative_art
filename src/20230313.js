029d5014cca2def06c448236910907a4310a459b
negiandleek <negiandleek@gmail.com>
2023-03-15 09:59:03 +0900

    20230315

--- src/20230313.js
+++ src/20230313.js
@@ -1,9 +1,15 @@
+// ‘SOUND OF DTM MEDAN VOLUME 1 (Fahrezi DTM)’ by DTM MEDAN is on 
+// https://on.soundcloud.com/5nsFB 
+
 let song, fft;
+
 
 function preload() {
   song = loadSound('./sound.mp3');
 }
 
+const canvasWidth = 400;
+const canvasHeight = 400;
 function setup() {
   createCanvas(400, 400);
   fft = new p5.FFT();
@@ -14,17 +20,21 @@ function draw() {
   background(0);
 
   let waveform = fft.waveform();
+  
   noFill();
   beginShape();
   stroke('#fb8500');
 
-  for (let i = 0; i < waveform.length; i+=50) {
-    let x = map(i, 0, waveform.length, 0, width);
-    let y = map(waveform[i], -1, 1, height, 0);
-    curveVertex(x, y);
+  beginShape();
+  for (let i = 0; i < waveform.length; i++) {
+    let angle = map(i, 0, waveform.length, 0, TWO_PI)
+    let radius = map(waveform[i], -1, 1, 0, height / 2);
+    let x = (canvasWidth / 2) + radius * cos(angle);
+    let y = (canvasHeight / 2) + radius * sin(angle);
+    vertex(x, y);
   }
-
-  endShape();
+  endShape(CLOSE);
+  
   
   // text('Click to play/pause', 20, 20);
 }

7067c0455d2fcf256ed57c4bfdf4e86003616fdc
negiandleek <negiandleek@gmail.com>
2023-03-27 22:22:15 +0900

    20230327

--- src/sandbox/20230326.ts
+++ src/sandbox/20230326.ts
@@ -1,7 +1,50 @@
 let audio: p5.SoundFile;
-const m = 50;
-const n = 50;
 let fft: p5.FFT;
+let counter = 0;
+
+
+let bassWaveform: Array<any>;
+let midWaveform: Array<any>;
+let highWaveform: Array<any>;
+
+function drawWaveform(p: p5, waveform: Array<any>, translateY: number = 0){
+  p.push()
+  p.translate(0, ((translateY + 1) * 3) * p.height / 11)
+  p.beginShape();
+  p.noFill()
+  p.strokeWeight(14)
+
+  // const groupSize = waveform.length;
+  // const step = Math.floor(waveform.length / groupSize);
+  // p.curveVertex(-9p.width / 6 + p.width / 2, p.height / 2);
+  // p.curveVertex(325, 0);
+  // p.curveVertex(325, 0);
+
+  for (let i = 0; i < waveform.length; i++) {
+    // let sum = 0;
+    // for (let j = 0; j < step; j++) {
+    //   let index = i * step + j;
+    //   sum += waveform[index];
+    // }
+    let x = Math.floor(p.map(i, 0, waveform.length, 0, p.width));
+    let y = Math.floor(p.map(waveform[i], -1, 1, 0, p.height / 8));
+    if (i % 2 == 0) {
+      y *= -1;
+    }
+    p.curveVertex(x, y);
+  }
+  // p.curveVertex(800, 0);
+  // p.curveVertex(800, 0);
+
+  p.endShape();
+  p.pop()
+}
+
+function mirrorSpectrum(p: p5, spectrum: Array<any>){
+  const invertedSpectrum = spectrum.slice().reverse();
+  const values = invertedSpectrum.concat(spectrum);
+  return values;
+}
 
 export const sketch = (p: p5) => {
   p.preload = () => {
@@ -9,20 +52,23 @@ export const sketch = (p: p5) => {
   }
   p.setup = () => {
     p.createCanvas(1200, 800)
-    fft = new p5.FFT();
+    fft = new p5.FFT(0.935, 16);
+    fft.smooth(0.8)
     fft.setInput(audio);
   };
 
   p.draw = () => {
   	p.background(220);
-    fft.analyze()
-    let trebEnergy = fft.getEnergy("treble");
-    let midEnergy= fft.getEnergy("mid");
-    let bassEnergy = fft.getEnergy("bass");
+    const spectrum = fft.waveform()
+    bassWaveform = mirrorSpectrum(p, spectrum.slice(0, Math.floor(spectrum.length * 0.3)))
+    midWaveform = mirrorSpectrum(p, spectrum.slice(Math.floor(spectrum.length * 0.3), Math.floor(spectrum.length * 0.6)))
+    highWaveform = mirrorSpectrum(p, spectrum.slice(Math.floor(spectrum.length * 0.6), spectrum.length));
+    drawWaveform(p, bassWaveform, 0)
+    drawWaveform(p, midWaveform, 1)
+    drawWaveform(p, highWaveform, 2)
+    // drawWaveform(p, mirrorSpectrum(p, spectrum.slice()))
 
-    p.ellipse(120,120,trebEnergy)
-    p.ellipse(360,120,midEnergy)
-    p.ellipse(600,120,bassEnergy)
+    counter += 1;
   };
 
 	p.mouseClicked = () => {

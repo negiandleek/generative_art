29effd3d5662b8950e2c196eaa3dc8ca39b90511
negiandleek <negiandleek@gmail.com>
2023-03-28 23:22:13 +0900

    20230328

--- src/sandbox/20230326.ts
+++ src/sandbox/20230326.ts
@@ -1,49 +1,56 @@
 let audio: p5.SoundFile;
 let fft: p5.FFT;
 let counter = 0;
+let pitch: number = 0;
 
-
-let bassWaveform: Array<any>;
-let midWaveform: Array<any>;
-let highWaveform: Array<any>;
-
-function drawWaveform(p: p5, waveform: Array<any>, translateY: number = 0){
+function drawPitch(p: p5, pitch: number, translate: {x?: number, y?:number, rotate?: number}, reverse: boolean = false){
+  const translateX = translate.x ?? 0;
+  const translateY = translate.y ?? 0;
+  const rotate = translate.rotate ?? 0
+  const minVertex = 0
+  const maxVertex = 16;
+  let vertex = Math.floor(p.map(pitch, 0, 22050, minVertex, maxVertex));
+  if(vertex % 2 == 0){
+    vertex += 1;
+  }
   p.push()
-  p.translate(0, ((translateY + 1) * 3) * p.height / 11)
+  p.translate(translateX, translateY)
+  p.rotate(rotate)
   p.beginShape();
   p.noFill()
   p.strokeWeight(14)
 
-  // const groupSize = waveform.length;
-  // const step = Math.floor(waveform.length / groupSize);
-  // p.curveVertex(-9p.width / 6 + p.width / 2, p.height / 2);
-  // p.curveVertex(325, 0);
-  // p.curveVertex(325, 0);
+  const startX = 50
+  const oneX = (p.width - 50) / vertex;
+  p.curveVertex(startX, 0);
+  p.curveVertex(startX, 0);
 
-  for (let i = 0; i < waveform.length; i++) {
-    // let sum = 0;
-    // for (let j = 0; j < step; j++) {
-    //   let index = i * step + j;
-    //   sum += waveform[index];
-    // }
-    let x = Math.floor(p.map(i, 0, waveform.length, 0, p.width));
-    let y = Math.floor(p.map(waveform[i], -1, 1, 0, p.height / 8));
+  for(let i = 1; i < vertex; i+=1){
+    let x = Math.floor(oneX * i);
+    let y = reverse ? -1 * p.height / 8 : p.height / 8;
     if (i % 2 == 0) {
       y *= -1;
     }
     p.curveVertex(x, y);
   }
-  // p.curveVertex(800, 0);
-  // p.curveVertex(800, 0);
-
+  
+  p.curveVertex(p.width - startX * 2, 0);
+  p.curveVertex(p.width - startX * 2, 0);
   p.endShape();
   p.pop()
 }
 
-function mirrorSpectrum(p: p5, spectrum: Array<any>){
-  const invertedSpectrum = spectrum.slice().reverse();
-  const values = invertedSpectrum.concat(spectrum);
-  return values;
+function getPitch(spectrum: Array<any>) {
+  let maxAmp = 0;
+  let maxIndex = 0;
+  for (let i = 0; i < spectrum.length; i++) {
+    if (spectrum[i] > maxAmp) {
+      maxAmp = spectrum[i];
+      maxIndex = i;
+    }
+  }
+  let freq = maxIndex * 22050 / spectrum.length;
+  return Math.round(freq);
 }
 
 export const sketch = (p: p5) => {
@@ -52,21 +59,38 @@ export const sketch = (p: p5) => {
   }
   p.setup = () => {
     p.createCanvas(1200, 800)
-    fft = new p5.FFT(0.935, 16);
+    p.background('#1A39C2')
+    fft = new p5.FFT(0.935, 64);
     fft.smooth(0.8)
     fft.setInput(audio);
   };
 
   p.draw = () => {
-  	p.background(220);
+  	p.background('#1A39C2');
     const spectrum = fft.waveform()
-    bassWaveform = mirrorSpectrum(p, spectrum.slice(0, Math.floor(spectrum.length * 0.3)))
-    midWaveform = mirrorSpectrum(p, spectrum.slice(Math.floor(spectrum.length * 0.3), Math.floor(spectrum.length * 0.6)))
-    highWaveform = mirrorSpectrum(p, spectrum.slice(Math.floor(spectrum.length * 0.6), spectrum.length));
-    drawWaveform(p, bassWaveform, 0)
-    drawWaveform(p, midWaveform, 1)
-    drawWaveform(p, highWaveform, 2)
-    // drawWaveform(p, mirrorSpectrum(p, spectrum.slice()))
+    if(counter % 4 == 0){
+      pitch = getPitch(spectrum.slice(0, Math.floor(spectrum.length)))
+    }
+    // p.blendMode(p.DARKEST)
+    {
+      p.stroke(0,0,0, 180)
+      drawPitch(p, pitch, {x: 20, y: 230})
+      p.stroke('#FF6F00')
+      drawPitch(p, pitch, {x: 0, y: 200})
+    }
+    {
+      p.stroke(0,0,0, 180)
+      drawPitch(p, pitch, {x: 20, y: 530}, true)
+      p.stroke('#FF6F00')
+      drawPitch(p, pitch, {x: 0, y: 500}, true)
+    }
+    // {
+    //   p.stroke(0,0,0, 180)
+    //   drawPitch(p, pitch, {x: 100, y: 670, rotate: -0.5}, true)
+    //   p.stroke('#FF6F00')
+    //   drawPitch(p, pitch, {x: 80, y: 640, rotate: -0.5}, true)
+    // }
+    // p.blendMode(p.DARKEST)
 
     counter += 1;
   };
